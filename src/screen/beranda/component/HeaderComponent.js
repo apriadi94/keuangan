8b59508940454b70b4d40196d0f27ab07ee280f5
Apriadi <dev.apriadi@gmail.com>
2021-07-06 18:36:55 +0700

    design header

--- src/screen/beranda/component/HeaderComponent.js
+++ src/screen/beranda/component/HeaderComponent.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 const HeaderComponent = () => {
     return(
-        <View style={{ backgroundColor : '#1ab2ff', height: 100}}>
+        <View style={{ backgroundColor : '#1ab2ff', height: 80}}>
             <View style={{ marginTop: 10, marginHorizontal: 5, flexDirection: 'row'}}>
-                <View style={{ justifyContent: 'flex-start' }}>
-                    <Text style={{color: 'white', fontSize: 18}}>Jan 2021</Text>
+                <View style={{ justifyContent: 'center', height: 60, marginLeft: 10 }}>
+                    <Text style={{color: 'white'}}>Saldo</Text>
+                    <Text style={{color: 'white', fontSize: 25, fontWeight: 'bold'}}>Rp. 3.500.000</Text>
                 </View>
                 <View style={{ flex: 1, alignItems: 'flex-end' }}>
-                    <Text style={{color: 'white', fontSize: 18}}>Jan 2021</Text>
+                    <View style={{ flexDirection: 'row'}}>
+                        <Icon name="chevron-left" size={16} color="#fff" style={{ marginTop: 5, marginRight: 10 }} />
+                        <Text style={{color: 'white', fontSize: 18}}>Jan 2021</Text>
+                        <Icon name="chevron-right" size={16} color="#fff" style={{ marginTop: 5, marginLeft: 10 }} />
+                    </View>
+                    <View style={{ flex: 1, justifyContent: 'flex-end'}}>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Icon name="filter" size={16} color="#fff" style={{ marginTop: 5, marginRight: 5 }} />
+                            <Text style={{color: 'white', fontSize: 18}}>Setiap Bulan</Text>
+                        </View>
+                    </View>
                 </View>
             </View>
         </View>
