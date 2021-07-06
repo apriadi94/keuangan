1b9b6dc314737a331f50c53762deacb39aed80a9
Apriadi <dev.apriadi@gmail.com>
2021-07-06 20:21:39 +0700

    design component list keuangan

--- src/screen/beranda/navigasi/HarianComponent.js
+++ src/screen/beranda/navigasi/HarianComponent.js
@@ -4,9 +4,29 @@ import { View, Text } from 'react-native'
 const HarianComponent = () => {
     return(
         <View>
-            <Text>
-                Text
-            </Text>
+            <View style={{ backgroundColor: '#fff', marginTop: 5 }}>
+                <View style={{marginTop: 10, marginHorizontal: 5}}>
+                    <View style={{flexDirection: 'row', marginBottom: 5}}>
+                        <View style={{ marginLeft : 10}}>
+                            <Text style={{ fontWeight: 'bold', fontSize: 25}}>06</Text>
+                        </View>
+                        <View style={{ marginLeft: 10, marginTop: 3 }}>
+                            <Text style={{fontSize: 10}}>
+                                07 2021
+                            </Text>
+                            <View style={{ backgroundColor: 'gray', alignItems: 'center' }}>
+                                <Text style={{fontSize: 10, color: 'white'}}>Selasa</Text>
+                            </View>
+                        </View>
+                        <View style={{ flex: 1, justifyContent: 'center' }}>
+                            <Text style={{ textAlign: 'right' }}>Rp. 0</Text>
+                        </View>
+                        <View style={{ flex: 1, justifyContent: 'center', marginRight: 10 }}>
+                            <Text style={{ textAlign: 'right' }}>Rp. 0</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
         </View>
     )
 }
