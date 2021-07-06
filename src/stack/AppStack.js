7d7a472c897b42d7554ad7d82370d773774adef6
Apriadi <dev.apriadi@gmail.com>
2021-07-06 19:01:48 +0700

    menambahkan bottom menu pada appStack

--- src/stack/AppStack.js
+++ src/stack/AppStack.js
@@ -1,17 +1,26 @@
 import 'react-native-gesture-handler'
 import React from 'react'
+import { Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import HomeStack from './HomeStack'
-import { createStackNavigator } from '@react-navigation/stack';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const AppStack = () => {
     return(
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeStack} options={{ headerShown: false }}/>
-            </Stack.Navigator>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeStack} options={{ 
+                    tabBarLabel: () => (
+                        <Text style={{fontSize : 12}}>Home</Text>
+                    ),
+                    tabBarIcon: ({ color }) => (
+                        <Icon name="home" size={20} color={color} style={{marginTop : 5}} />
+                    ),
+                 }}/>
+            </Tab.Navigator>
         </NavigationContainer>
     )
 }
