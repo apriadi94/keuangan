8284007e6d7c52b9cda3542ae154814ed5a8a960
Apriadi <dev.apriadi@gmail.com>
2021-07-07 09:33:23 +0700

    menghilangkan warning notif

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import AppStack from './src/stack/AppStack';
 import { AppProvider } from './src/provider/AppProvider';
+import 'moment/locale/id';
+import {LogBox } from 'react-native';
+LogBox.ignoreLogs(['Reanimated 2']);
 
 const App = () => {
   return (
