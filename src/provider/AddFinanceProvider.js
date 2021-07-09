604fe5d1be1a9dba409137d34c8b80417895e152
Apriadi <dev.apriadi@gmail.com>
2021-07-10 06:22:20 +0700

    mengubah beberapa logic

--- src/provider/AddFinanceProvider.js
+++ src/provider/AddFinanceProvider.js
@@ -2,8 +2,14 @@ import React, { useState, createContext } from 'react'
 
 export const AddFinanceContext = createContext()
 export const AddFinanceProvider = ({ children }) => {
+    const [active, setActive] = useState('pengeluaran')
+    const [kategori, setKategori] = useState({
+        kategoriId: 1,
+        kategoriName: 'Makanan',
+        kategoriJenis: 'pengeluaran'
+    })
 
-    const addFinanceState = {}
+    const addFinanceState = { kategori, setKategori, active, setActive }
     return(
         <AddFinanceContext.Provider value={addFinanceState}>
             {
