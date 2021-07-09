6e8be772459e9a0d54d3acd61ea1eea56f642b6d
Apriadi <dev.apriadi@gmail.com>
2021-07-09 20:49:11 +0700

    mengubah textinput date menjadi date picker

--- src/screen/beranda/TambahFinanceScreen.js
+++ src/screen/beranda/TambahFinanceScreen.js
@@ -1,8 +1,31 @@
 import React, { useState } from 'react'
-import { View, Text, TouchableOpacity, TextInput } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, TouchableWithoutFeedback } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import moment from 'moment'
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 
 const TambahFinanceScreen = () => {
     const [active, setActive] = useState('pengeluaran')
+    const [date, setDate] = useState(new Date());
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+
+    const onChangeDate = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(Platform.OS === 'ios');
+        setDate(currentDate);
+      };
+    
+      const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+      };
+    
+      const showDatepicker = () => {
+        showMode('date');
+      };
+
     return(
         <View>
             <View style={{ backgroundColor: '#fff' }}>
@@ -18,9 +41,16 @@ const TambahFinanceScreen = () => {
                 <View style={{ marginHorizontal: 10, marginBottom: 20 }}>
                     <View style={{ marginTop: 30, flexDirection: 'row', alignItems: 'center' }}>
                         <Text style={{ fontSize: 16, width: 120 }}>Tanggal</Text>
-                        <View style={{ marginRight: 10, borderColor: '#b3b3b3', borderWidth: 1, borderRadius: 10, flex: 1, height: 40 }}>
-                            <TextInput style={{ fontSize: 16, marginLeft: 10 }}/>
-                        </View>
+                        <TouchableWithoutFeedback onPress={showDatepicker}>
+                            <View style={{ flexDirection: 'row', marginRight: 10, borderColor: '#b3b3b3', borderWidth: 1, borderRadius: 10, flex: 1, height: 40 }}>
+                                <View style={{ flex: 1, marginLeft: 10, justifyContent: 'center' }}>
+                                    <Text style={{ fontSize: 16 }}>{moment(date).format('dddd, DD-MM-YYYY')}</Text>
+                                </View>
+                                <View style={{ justifyContent: 'center', alignItems: 'center', marginRight: 10 }}>
+                                    <Icon name="calendar" size={16} color={'black'}/>
+                                </View>
+                            </View>
+                        </TouchableWithoutFeedback>
                     </View>
                     <View style={{ marginTop: 10, flexDirection: 'row', alignItems: 'center' }}>
                         <Text style={{ fontSize: 16, width: 120 }}>kategori</Text>
@@ -48,6 +78,16 @@ const TambahFinanceScreen = () => {
                     <Text style={{ color: 'white', fontSize: 20 }}>Simpan</Text>
                 </View>
             </View>
+            {show && (
+                <DateTimePicker
+                testID="dateTimePicker"
+                value={date}
+                mode={mode}
+                is24Hour={true}
+                display="default"
+                onChange={onChangeDate}
+                />
+            )}
         </View>
     )
 }
